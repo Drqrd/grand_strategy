21e4bc77b84f6cd4e0d66f007a0df302bcc3fc61
justin_k_m <justinm0623@gmail.com>
2022-03-03 00:24:46 -0500

    Small extension for slicing arrays

---
+++ GameDev/Assets/Scripts/Extensions.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+
+public static class Extensions
+{
+    private static System.Random rng = new System.Random();
+    public static void Shuffle<T>(this IList<T> list)
+    {
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = rng.Next(n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
+
+    public static T[] Slice<T>(this T[] source, int start, int end)
+    {
+        if (end < 0)
+        {
+            end = source.Length + end;
+        }
+        int len = end - start;
+
+        T[] res = new T[len];
+        for (int i = 0; i < len; i++)
+        {
+            res[i] = source[i + start];
+        }
+        return res;
+    }
+}
+
+
+
