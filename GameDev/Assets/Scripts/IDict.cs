8c6dc9822acf2eaff4daf23050e49526f325b859
justin_k_m <justinm0623@gmail.com>
2022-03-15 01:34:32 -0400

    Update IDict.cs

--- GameDev/Assets/Scripts/IDict.cs
+++ GameDev/Assets/Scripts/IDict.cs
@@ -5,29 +5,7 @@ using static WorldData;
 
 namespace IDict
 {
-    public class EdgeCompareOverride : IEqualityComparer<Edge>
-    {
-        public bool Equals(Edge a, Edge b)
-        {
-            if (a == null && b == null) { return true; }
-            if (a == null || b == null) { return false; }
-
-            bool val = true;
-            for (int ind = 0; ind < a.edge.Length; ind++) { if (a.edge[ind].vertex != b.edge[ind].vertex) { val = false; } }
-            return val;
-        }
-
-        public int GetHashCode(Edge e)
-        {
-            int hc = e.edge.Length;
-            foreach (Point val in e.edge) 
-            {
-                int v = (int)Mathf.Floor(12 * val.vertex.x + 23 * val.vertex.y + 34 * val.vertex.z);
-                hc = unchecked(hc * 314159 + v); 
-            }
-            return hc;
-        }
-    }
+   
 }
 
 
